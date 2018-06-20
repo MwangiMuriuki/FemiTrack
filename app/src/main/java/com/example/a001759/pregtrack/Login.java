a5ca3bbbf808461cfb7f6d7295fba9ca3db6791b
Mwangi Muriuki <muriukimwangi.mm@gmail.com>
2018-06-20 17:41:14 +0300

    Commit 6. Thank You Lord!!!

--- app/src/main/java/com/example/a001759/pregtrack/Login.java
+++ app/src/main/java/com/example/a001759/pregtrack/Login.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class Login extends Activity {
     Button login;
+    ImageButton buttonFacebook, buttonTwitter,buttonGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,9 @@ public class Login extends Activity {
         setContentView(R.layout.activity_login);
 
         login = findViewById(R.id.login);
+        buttonFacebook = findViewById(R.id.facebook_sign_in);
+        buttonTwitter = findViewById(R.id.twitter_sign_in);
+        buttonGoogle = findViewById(R.id.google_sign_in);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +30,26 @@ public class Login extends Activity {
                 startActivity(intent);
             }
         });
+
+        buttonFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Sign In with Facebook", Toast.LENGTH_LONG).show();
+            }
+        });
+
+       buttonTwitter.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getApplicationContext(), "Sign in with Twitter", Toast.LENGTH_LONG).show();
+           }
+       });
+
+       buttonGoogle.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getApplicationContext(), "Sign in with Google", Toast.LENGTH_LONG).show();
+           }
+       });
     }
 }
